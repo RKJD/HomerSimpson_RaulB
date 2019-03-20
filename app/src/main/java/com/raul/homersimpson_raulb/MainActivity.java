d0b88414e81f7ad9c0f0e6b0b1a8d7b56503aa08
Raul Barrero <rbarrero1998@gmail.com>
2019-03-20 12:30:43 +0100

    Apartado F - Acabado

--- app/src/main/java/com/raul/homersimpson_raulb/MainActivity.java
+++ app/src/main/java/com/raul/homersimpson_raulb/MainActivity.java
@@ -5,6 +5,8 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     AnimationDrawable titulo_simpson;
     boolean bol = false;
     boolean bolMusica = false;
+    boolean bolAnime = false;
     MediaPlayer player;
 
      @Override
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         final ImageView ejeRojo =findViewById(R.id.ejeRojo);
         final ImageView ejeVerde =findViewById(R.id.ejeVerde);
         final ImageView ojo =findViewById(R.id.ojo);
+
         final ImageView donut =findViewById(R.id.donut);
          ejeAzul.setVisibility(View.INVISIBLE);
          ejeRojo.setVisibility(View.INVISIBLE);
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                     // quitar fotos
                     ejeAzul.setVisibility(View.INVISIBLE);
                     ejeVerde.setVisibility(View.INVISIBLE);
+                    paradeRotar(ojo);
                     ojo.setVisibility(View.INVISIBLE);
                     donut.setVisibility(View.INVISIBLE);
                     ejeRojo.setVisibility(View.INVISIBLE);
@@ -54,12 +59,14 @@ public class MainActivity extends AppCompatActivity {
                     // poner ftos
                     ejeRojo.setVisibility(View.VISIBLE);
                     ojo.setVisibility(View.VISIBLE);
+                    rotarImagen(ojo);
                     ejeAzul.setVisibility(View.VISIBLE);
                     ejeVerde.setVisibility(View.VISIBLE);
                     donut.setVisibility(View.VISIBLE);
 
                  }
             }
+
         });
          donut.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -123,5 +130,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         stopPlayer();
+    }
+
+
+    private void rotarImagen(View view) {
+        RotateAnimation animation = new RotateAnimation(50, -50,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        animation.setDuration(2000);
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setRepeatMode(Animation.REVERSE);
+        view.startAnimation(animation);
+    }
+    private void paradeRotar(View view) {
+        RotateAnimation animation = new RotateAnimation(0, 1,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        animation.setDuration(1);
+        animation.setRepeatCount(0);
+        animation.setRepeatMode(Animation.REVERSE);
+        view.startAnimation(animation);
+
     }
 }
