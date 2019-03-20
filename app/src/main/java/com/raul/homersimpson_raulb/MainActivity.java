c0a332cff14a81895457581a1938d4aebc149767
Raul Barrero <rbarrero1998@gmail.com>
2019-03-20 11:33:50 +0100

    Apartado B - pero el titulo sale detras de otro titulo codigo correcto o deberia

--- app/src/main/java/com/raul/homersimpson_raulb/MainActivity.java
+++ app/src/main/java/com/raul/homersimpson_raulb/MainActivity.java
@@ -1,14 +1,24 @@
 package com.raul.homersimpson_raulb;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable titulo_simpson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test commit pre examen
+        ImageView titulo=findViewById(R.id.titulo_simpson);
+        titulo.setBackgroundResource(R.drawable.titulo);
+        titulo_simpson = (AnimationDrawable) titulo.getBackground();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean bol) {
+        super.onWindowFocusChanged(bol);
+        titulo_simpson.start();
     }
 }
